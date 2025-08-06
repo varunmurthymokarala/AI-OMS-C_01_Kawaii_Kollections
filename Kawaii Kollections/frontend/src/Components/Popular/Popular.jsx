23f9700cd96ccb7b3d9388b396ed2d77ad71eea0
sai kiran <saikirankoluguri@gmail.com>
2025-08-06 09:47:28 -0500

    updating images and adding css to footer

--- Kawaii Kollections/frontend/src/Components/Popular/Popular.jsx
+++ Kawaii Kollections/frontend/src/Components/Popular/Popular.jsx
@@ -5,7 +5,7 @@ import Item from '../Item/Item'; // Adjust the path as necessary
 const Popular = () => {
   return (
     <div className='popular'>
-        <h1>POPULAR IN WOMEN</h1>
+        <h1>POPULAR TOYS</h1>
         <hr />
         <div className="popular-item">
             {data_product.map((item, i) => {
