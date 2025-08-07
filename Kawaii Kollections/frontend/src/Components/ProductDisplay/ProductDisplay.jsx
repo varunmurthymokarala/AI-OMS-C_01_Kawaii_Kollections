50c859119cc01f74f8b315ffa77ef649d9b4a68e
sai kiran <saikirankoluguri@gmail.com>
2025-08-06 22:18:42 -0500

    added Breadcrumps and productdisplay pages

---
+++ Kawaii Kollections/frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './ProductDisplay.css'
+
+import start_icon   from '../Assets/star_icon.png'
+import star_dull_icon from '../Assets/star_dull_icon.png'
+
+export const ProductDisplay = (props) => {
+    const {product}=props;    
+  return (
+    <div className='productdisplay'>
+        <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.image} alt=''/>
+                <img src={product.image} alt=''/>
+                <img src={product.image} alt=''/>
+                <img src={product.image} alt=''/>
+            </div>
+            <div className="productdisplay-img">
+                <img className="productdisplay-main-img" src={product.image} alt=''/>
+            </div>
+        </div>
+        <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-stars">
+                <img src={start_icon} alt=''/>
+                <img src={start_icon} alt=''/>
+                <img src={start_icon} alt=''/>
+                <img src={start_icon} alt=''/>
+                <img src={star_dull_icon} alt=''/>
+                <p>(134)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                 <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-discription">
+                Kendama is a traditional Japanese skill toy, essentially a cup-and-ball game, 
+                consisting of a handle (ken), a ball (tama), and a string connecting them
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Color</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>Yellow</div>
+                    <div>Green</div>
+                    <div>White</div>
+                    <div>Pink</div>
+                    <div>Black</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className="productdisplay-category"><span>Category:</span>{product.category},Japnese toy,gifts</p>
+            <p className="productdisplay-category"><span>Tags:</span>Latest,Animi,Gifts</p>
+
+        </div>
+    </div>
+  )
+}
